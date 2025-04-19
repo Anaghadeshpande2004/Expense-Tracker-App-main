28051d936a11b93a7976a3247818d03017db8b94
Anaghadeshpande2004 <anaghadeshpande2004@gmail.com>
2025-04-19 09:11:52 +0530

    ninth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import "./App.css";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Login from './Pages/Auth/Login';
 import Register from './Pages/Auth/Register';
-// App.js
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home';
 import SetAvatar from './Pages/Avatar/setAvatar';
+import axios from 'axios'; // ✅ important
 
-
+axios.defaults.baseURL = 'https://expense-tracker-app-main-backend.vercel.app';
+axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
-    
-      <div className="App">
+    <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -22,8 +23,8 @@ const App = () => {
           <Route path="/setAvatar" element={<SetAvatar />} />
         </Routes>
       </BrowserRouter>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
